ab11d99e6f1bde8ce17ec48cbc0198ca820cb5ce
Mikael Kunzendorf Nissen <mikael@followcamsolutions.com>
2024-12-22 12:32:04 +0100

    corrected structure handle c

--- src/generators/structure_handle_c_generator.js
+++ src/generators/structure_handle_c_generator.js
@@ -5,20 +5,16 @@
 
 'use strict';
 
-// ####################### structure_handle_c.h generation ####################### //
-
 function structure_handle_c_generator(form, od, indexes) {
-    let header = `// structure_handle.h
-#include <stdint.h>
-#ifndef STRUCTURE_HANDLE_C_H
+    let header = `#ifndef STRUCTURE_HANDLE_C_H
 #define STRUCTURE_HANDLE_C_H
+
+#include <linux/mutex.h>
+#include <linux/uaccess.h>
+#include "ioctl_lan9252.h"
 
 #pragma pack(push, 1)  // Align structures to 1-byte boundaries
 
-`;
-
-    // Define a structure
-    header += `// Define a structure
 `;
 
     // Define input_structure
@@ -26,10 +22,10 @@ function structure_handle_c_generator(form, od, indexes) {
 `;
     indexes.forEach(index => {
         const objd = od[index];
-        if (objd.pdo_mappings && objd.pdo_mappings.includes(txpdo)) {
-            const varName = capitalizeFirstLetter(variableName(objd.name));
+        if (objd.pdo_mappings && objd.pdo_mappings.includes('txpdo')) {
+            const varName = variableName(objd.name);
             const ctype = getCType(objd.dtype);
-            inputStruct += `    ${ctype}\t${varName};
+            inputStruct += `    ${ctype} ${varName};
 `;
         }
     });
@@ -42,10 +38,10 @@ function structure_handle_c_generator(form, od, indexes) {
 `;
     indexes.forEach(index => {
         const objd = od[index];
-        if (objd.pdo_mappings && objd.pdo_mappings.includes(rxpdo)) {
-            const varName = capitalizeFirstLetter(variableName(objd.name));
+        if (objd.pdo_mappings && objd.pdo_mappings.includes('rxpdo')) {
+            const varName = variableName(objd.name);
             const ctype = getCType(objd.dtype);
-            outputStruct += `    ${ctype}\t${varName};
+            outputStruct += `    ${ctype} ${varName};
 `;
         }
     });
@@ -53,13 +49,121 @@ function structure_handle_c_generator(form, od, indexes) {
 
 `;
 
-    // Closing the header with pragma pack and header guard
-    header += inputStruct + outputStruct;
+    // Extern declarations and mutexes
+    let externDeclarations = `
+extern struct input_structure ethercat_input_structure;
+extern struct output_structure ethercat_output_structure;
+extern struct mutex ethercat_input_structure_mutex;
+extern struct mutex ethercat_output_structure_mutex;
 
-    header += `#pragma pack(pop)      // Restore default alignment
-
-#endif  // STRUCTURE_HANDLE_C_H
 `;
+
+    // Function to handle input data
+    let inputDataHandleFunc = `void input_data_handle(struct input_structure *data, uint8_t *ethercat_input) {
+    uint8_t offset = 3; // Adjust offset as needed
+`;
+    indexes.forEach(index => {
+        const objd = od[index];
+        if (objd.pdo_mappings && objd.pdo_mappings.includes('txpdo')) {
+            const varName = variableName(objd.name);
+            inputDataHandleFunc += `    memcpy(ethercat_input + offset, &data->${varName}, sizeof(data->${varName}));
+    offset += sizeof(data->${varName});
+`;
+        }
+    });
+    inputDataHandleFunc += `}
+
+`;
+
+    // Function to handle output data
+    let outputDataHandleFunc = `void output_data_handle(struct output_structure *data, uint8_t *ethercat_output_data) {
+    uint8_t offset = 0;
+`;
+    indexes.forEach(index => {
+        const objd = od[index];
+        if (objd.pdo_mappings && objd.pdo_mappings.includes('rxpdo')) {
+            const varName = variableName(objd.name);
+            outputDataHandleFunc += `    memcpy(&data->${varName}, ethercat_output_data + offset, sizeof(data->${varName}));
+    offset += sizeof(data->${varName});
+`;
+        }
+    });
+    outputDataHandleFunc += `}
+
+`;
+
+    // Function lan9252_ioctl
+    let ioctlFunc = `static long int lan9252_ioctl(struct file *file, unsigned cmd, unsigned long arg){
+
+    switch(cmd){
+`;
+    // Common cases
+    ioctlFunc += `        case WR_VALUE:
+            mutex_lock(&ethercat_input_structure_mutex);
+            if(copy_from_user(&ethercat_input_structure, (struct input_structure *) arg, sizeof(ethercat_input_structure)))
+                printk("ioctl - Error copying data from user!\\n");
+            mutex_unlock(&ethercat_input_structure_mutex);
+            break;
+
+        case RD_VALUE:
+            mutex_lock(&ethercat_output_structure_mutex);
+            if(copy_to_user((struct output_structure *) arg, &ethercat_output_structure, sizeof(ethercat_output_structure)))
+                printk("ioctl - Error copying data to user!\\n");
+            mutex_unlock(&ethercat_output_structure_mutex);
+            break;
+
+`;
+
+    // Generate write cases for input_structure variables
+    indexes.forEach((index, idx) => {
+        const objd = od[index];
+        if (objd.pdo_mappings && objd.pdo_mappings.includes('txpdo')) {
+            const varName = variableName(objd.name);
+            const caseName = `WR_VALUE_${varName.toUpperCase()}`;
+            ioctlFunc += `        case ${caseName}:
+            mutex_lock(&ethercat_input_structure_mutex);
+            if(copy_from_user(&ethercat_input_structure.${varName}, (${getCType(objd.dtype)} *) arg, sizeof(ethercat_input_structure.${varName})))
+                printk("ioctl - Error copying data from user!\\n");
+            mutex_unlock(&ethercat_input_structure_mutex);
+            break;
+
+`;
+        }
+    });
+
+    // Generate read cases for output_structure variables
+    indexes.forEach((index, idx) => {
+        const objd = od[index];
+        if (objd.pdo_mappings && objd.pdo_mappings.includes('rxpdo')) {
+            const varName = variableName(objd.name);
+            const caseName = `RD_VALUE_${varName.toUpperCase()}`;
+            ioctlFunc += `        case ${caseName}:
+            mutex_lock(&ethercat_output_structure_mutex);
+            if(copy_to_user((${getCType(objd.dtype)} *) arg, &ethercat_output_structure.${varName}, sizeof(ethercat_output_structure.${varName})))
+                printk("ioctl - Error copying data to user!\\n");
+            mutex_unlock(&ethercat_output_structure_mutex);
+            break;
+
+`;
+        }
+    });
+
+    ioctlFunc += `        default:
+            return -EINVAL;
+    }
+    return 0;
+}
+
+`;
+
+    // Close header guard and pragma
+    let footer = `#pragma pack(pop)      // Restore default alignment
+
+#endif // STRUCTURE_HANDLE_C_H
+`;
+
+    // Combine all parts
+    header += inputStruct + outputStruct + externDeclarations + inputDataHandleFunc + outputDataHandleFunc + ioctlFunc + footer;
 
     return header;
 
@@ -67,16 +171,16 @@ function structure_handle_c_generator(form, od, indexes) {
 
     function getCType(dtype) {
         switch (dtype) {
-            case DTYPE.UNSIGNED8: return 'uint8_t';
-            case DTYPE.UNSIGNED16: return 'uint16_t';
-            case DTYPE.UNSIGNED32: return 'uint32_t';
-            case DTYPE.UNSIGNED64: return 'uint64_t';
-            case DTYPE.INTEGER8: return 'int8_t';
-            case DTYPE.INTEGER16: return 'int16_t';
-            case DTYPE.INTEGER32: return 'int32_t';
-            case DTYPE.INTEGER64: return 'int64_t';
-            case DTYPE.REAL32: return 'float';
-            case DTYPE.REAL64: return 'double';
+            case 'UNSIGNED8': return 'uint8_t';
+            case 'UNSIGNED16': return 'uint16_t';
+            case 'UNSIGNED32': return 'uint32_t';
+            case 'UNSIGNED64': return 'uint64_t';
+            case 'INTEGER8': return 'int8_t';
+            case 'INTEGER16': return 'int16_t';
+            case 'INTEGER32': return 'int32_t';
+            case 'INTEGER64': return 'int64_t';
+            case 'REAL32': return 'float';
+            case 'REAL64': return 'double';
             // Add more cases as needed
             default: return 'uint32_t'; // Default type
         }
@@ -84,10 +188,7 @@ function structure_handle_c_generator(form, od, indexes) {
 
     function variableName(name) {
         // Remove invalid characters and replace spaces with underscores
-        return name.replace(/\s+/g, '').replace(/[^\w]/g, '');
+        return name.replace(/\s+/g, '_').replace(/[^\w]/g, '');
     }
 
-    function capitalizeFirstLetter(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    }
 } 

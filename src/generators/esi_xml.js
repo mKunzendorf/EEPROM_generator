515165e4dc800d8b8702705cd2e3894a2d123cff
Mikael Kunzendorf Nissen <mikael@followcamsolutions.com>
2025-07-03 11:45:21 +0200

    removed spaces from device physics

--- src/generators/esi_xml.js
+++ src/generators/esi_xml.js
@@ -30,7 +30,7 @@ function esi_generator(form, od, indexes, dc)
 	//Groups
 	esi += `    <Groups>\n      <Group>\n        <Type>${form.TextGroupType.value}</Type>\n        <Name LcId="1033">${form.TextGroupName5.value}</Name>\n      </Group>\n    </Groups>\n    <Devices>\n`;
 	//Physics  
-	esi += `      <Device Physics="${form.Port0Physical.value + form.Port1Physical.value + form.Port2Physical.value || + form.Port3Physical.value}">\n        <Type ProductCode="#x${parseInt(form.ProductCode.value).toString(16)}" RevisionNo="#x${parseInt(form.RevisionNumber.value).toString(16)}">${form.TextDeviceType.value}</Type>\n`;
+	esi += `      <Device Physics="${(form.Port0Physical.value + form.Port1Physical.value + form.Port2Physical.value + form.Port3Physical.value).replace(/\s+/g, '')}">\n        <Type ProductCode="#x${parseInt(form.ProductCode.value).toString(16)}" RevisionNo="#x${parseInt(form.RevisionNumber.value).toString(16)}">${form.TextDeviceType.value}</Type>\n`;
 	//Add  Name info
 	esi += `        <Name LcId="1033">${form.TextDeviceName.value}</Name>\n`;
 	//Add in between
